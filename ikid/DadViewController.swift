dc10354a5cb3877d3cd83b423bddf092541e1fd3
changz23-1836696 <43832007+changz23-1836696@users.noreply.github.com>
2021-05-04 23:58:01 -0700

    dad

---
+++ ikid/DadViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DadViewController.swift
+//  ikid
+//
+//  Created by 曾畅 on 5/4/21.
+//
+
+import UIKit
+
+class DadViewController: UIViewController {
+    
+    var dadFaceViewController : DadFaceViewController! = nil
+    var dadPunchViewController : DadPunchViewController! = nil
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        punchlineBuilder()
+        faceBuilder()
+        swiftViewController(self, to: dadFaceViewController)
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func switchView(_ sender: Any) {
+        
+        if dadFaceViewController != nil && dadFaceViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            dadPunchViewController.view.frame = view.frame
+            swiftViewController(dadFaceViewController, to: dadPunchViewController)
+        }else{
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            dadFaceViewController.view.frame = view.frame
+            swiftViewController(dadPunchViewController, to: dadFaceViewController)
+        }
+    }
+    
+    fileprivate func swiftViewController(_ from : UIViewController?, to : UIViewController?){
+        if from != nil {
+            from!.willMove(toParent: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParent()
+        }
+        
+        if to != nil {
+            self.addChild(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParent: self)
+        }
+    }
+    
+    fileprivate func punchlineBuilder() {
+        if dadPunchViewController == nil {
+            dadPunchViewController = storyboard?.instantiateViewController(withIdentifier: "dad2") as! DadPunchViewController
+        }
+    }
+    
+    fileprivate func faceBuilder() {
+        if dadFaceViewController == nil {
+            dadFaceViewController = storyboard?.instantiateViewController(withIdentifier: "dad1") as! DadFaceViewController
+        }
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
